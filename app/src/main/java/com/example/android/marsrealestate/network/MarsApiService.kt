8813c2654a557a8927dff8df33bd64f57c020d38
Jinyan Liu <jinyan.liu@truecaller.com>
2020-03-20 21:26:19 +0100

    Implement option menu

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -21,10 +21,10 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
@@ -40,7 +40,7 @@ private val retrofit = Retrofit.Builder()
 
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties():
+    fun getProperties(@Query("filter") type:String):
             Deferred<List<MarsProperty>>
 }
 
@@ -48,6 +48,12 @@ object MarsApi {
     val retrofitService: MarsApiService by lazy {
         retrofit.create(MarsApiService::class.java)
     }
+}
+
+enum class MarsApiFilter(val value: String) {
+    SHOW_RENT("rent"),
+    SHOW_BUY("buy"),
+    SHOW_ALL("all")
 }
 
 
